fb7a0f98a707053b53c90fa7669f06d983fbd33e
Justin Duch <aphelion030@gmail.com>
2018-07-24 16:56:12 +1000

    new sql model

--- conf/viperidae.sql
+++ conf/viperidae.sql
@@ -1,44 +1,47 @@
--- MySQL dump 10.16  Distrib 10.3.8-MariaDB, for osx10.13 (x86_64)
---
--- Host: localhost    Database: viperidae
--- ------------------------------------------------------
--- Server version	10.3.8-MariaDB
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.5-10.3.8-MariaDB)
+# Database: viperidae
+# Generation Time: 2018-07-24 06:54:55 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `Clients`
---
+
+# Dump of table Clients
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `Clients`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Clients` (
   `client_id` varchar(56) NOT NULL DEFAULT '''''',
   `client_secret` varchar(56) NOT NULL DEFAULT '''''',
   `name` varchar(56) NOT NULL DEFAULT '''''',
+  `website` varchar(256) NOT NULL DEFAULT '''''',
   `description` varchar(256) NOT NULL DEFAULT '''''',
   `user_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `Tokens`
---
+
+
+# Dump of table Tokens
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `Tokens`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Tokens` (
   `token_id` varchar(56) NOT NULL DEFAULT '''''',
   `refresh_token` varchar(56) NOT NULL DEFAULT '''''',
@@ -46,15 +49,14 @@ CREATE TABLE `Tokens` (
   `client_id` varchar(56) NOT NULL DEFAULT '''''',
   PRIMARY KEY (`token_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `Users`
---
+
+
+# Dump of table Users
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `Users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `Users` (
   `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(56) NOT NULL DEFAULT '''''',
@@ -62,15 +64,13 @@ CREATE TABLE `Users` (
   `email` varchar(120) NOT NULL DEFAULT '''''',
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-07-22 15:26:34
